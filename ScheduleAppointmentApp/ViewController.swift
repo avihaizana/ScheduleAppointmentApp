0bf74d5e8e81c01a1267c78cbfc90b859a50774f
user140459 <user140459@eu234.local>
2018-05-25 14:29:07 +0100

    Initial Commit

---
+++ ScheduleAppointmentApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScheduleAppointmentApp
+//
+//  Created by user140459 on 5/25/18.
+//  Copyright © 2018 Avihai Zana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
